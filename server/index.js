dbbf40d35dac0ae4ccb7eb9f4451833da02d8cab
Julian <juliankimmings@gmail.com>
2020-06-24 15:31:39 +0100

    create local proxy for dwp api

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+const axios = require('axios').default;
+
+const port = process.env.PORT || 3000;
+
+const apiBase = 'https://bpdts-test-app.herokuapp.com'
+app.use(helmet());
+
+app.get('/users', (req, res) => {
+  console.log('get users');
+  axios.get(`${apiBase}/users`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.send({ error });
+    });
+});
+
+app.get('/city-users/:city', (req, res) => {
+  axios.get(`${apiBase}/city/${req.params.city}/users`)
+    .then(results => {
+      const inRadius = results.data.filter(item => {
+        const distance = haversine(item.latitude, item.longitude, london.latitude, london.longitude, 'M');
+        return distance < london.radius + 50;
+      });
+
+      let str = '';
+      for(var i=0; i<inRadius.length; i++) {
+        str += `${inRadius[i].longitude}, ${inRadius[i].latitude}\n`;
+      }
+      res.send(str);
+    })
+    .catch(error => {
+      console.log('error', error);
+      res.send('An error occoured. That request was not supported.');
+    });
+});
+
+//app.use('/', express.static('./build'));
+
+/* catch all */
+app.get('*', (req, res) => {
+  res
+    .status(404)
+    .send('Not sure what you are looking for?');
+});
+
+app.listen(port, () => console.log(`App listening on ${port}`))
--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import haversine from './haversine';
 
-export const apiBase = 'https://bpdts-test-app.herokuapp.com';
+export const apiBase = '/api';
 export const london = {
   latitude: 51.5074,
   longitude: 0.1278,
