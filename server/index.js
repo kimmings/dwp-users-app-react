d86d441c571bb0a4ede9fc5b87212205fababe68
Julian <juliankimmings@gmail.com>
2020-06-26 11:54:22 +0100

    Prettier formatting

--- server/index.js
+++ server/index.js
@@ -9,27 +9,29 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-const apiBase = 'https://bpdts-test-app.herokuapp.com'
+const apiBase = 'https://bpdts-test-app.herokuapp.com';
 app.use(helmet());
 
 app.get('/users', (req, res) => {
   console.log('get users');
-  axios.get(`${apiBase}/users`)
+  axios
+    .get(`${apiBase}/users`)
     .then(({ data }) => {
       res.send(data);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
       res.send({ error });
     });
 });
 
 app.get('/city/:city/users', (req, res) => {
-  axios.get(`${apiBase}/city/${req.params.city}/users`)
-    .then( ({ data }) => {
+  axios
+    .get(`${apiBase}/city/${req.params.city}/users`)
+    .then(({ data }) => {
       res.send(data);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log('error', error);
       res.send('An error occoured. That request was not supported.');
     });
@@ -39,9 +41,7 @@ app.use(express.static('./build'));
 
 /* catch all */
 app.get('*', (req, res) => {
-  res
-    .status(404)
-    .send('Not sure what you are looking for?');
+  res.status(404).send('Not sure what you are looking for?');
 });
 
-app.listen(port, () => console.log(`App listening on ${port}`))
+app.listen(port, () => console.log(`App listening on ${port}`));
